9ed888b7d70799fe712dedfeccaeb737f169eaa1
FelipeACX <juanfe1190@gmail.com>
2022-01-22 17:32:57 -0500

    First stable backend version created

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,3 +1,4 @@
+const { default: axios } = require("axios");
 const { Pool } = require("pg")
 
 const pool = new Pool({
@@ -48,6 +49,7 @@ const loginRequest = async (req, res) => {
         message: "Login successfully",
         body: {
           user: {
+            "id": response.rows[0].id,
             "name": response.rows[0].name,
             "email": response.rows[0].email,
             "rol": response.rows[0].rol
@@ -82,16 +84,95 @@ const logoutRequest = async (req, res) => {
   })
 }
 
+const getBestPelis = async (req, res) => {
+  try {
+    const page = req.query.page
+    const queryParameters = {
+      'api_key': '0d677b16a44d2b5a79edf325bc1ee9b7',
+      'language': 'es-CO',
+      'sort_by': 'popularity.desc',
+      'page': page,
+    };
+    const response = await axios.get("https://api.themoviedb.org/3/discover/movie",
+      {
+        params: queryParameters
+      }
+    )
+    res.json({
+      status: 200,
+      successful: true,
+      message: "Data collected successfully",
+      body: {
+        pelis: response.data
+      }
+    })
+  } catch (error) {
+    res.json({
+      status: 400,
+      successful: false,
+      message: "Error: "+error,
+      body: {}
+    })
+  }
+}
+
 const newFavorite = async (req, res) => {
+  try {
+    const { 
+      user_id,
+      title,
+      release_date,
+      vote_average,
+      vote_count,
+      poster_path,
+      overview
+    } = req.body
+    //Get user to validate credentials
+    await pool.query("insert into Users (user_id,title,release_date,vote_average,vote_count,poster_path,overview) values ($1,$2,$3,$4,$5,$6,$7);", 
+    [user_id, title, release_date, vote_average, vote_count, poster_path, overview])
+    res.json({
+      status: 200,
+      successful: true,
+      message: "New favorite movie added successfully",
+      body: {
+        movie: title
+      }
+    })
+  } catch (error) {
+    res.json({
+      status: 400,
+      successful: false,
+      message: "Error: " + error,
+      body: {}
+    })
+  }
 }
 
 const getFavorites = async (req, res) => {
-}
-
-const getBestPelis = async (req, res) => {
+  try {
+    const user_id = req.query.user_id
+    const response = await pool.query("select * from Pelis where user_id = $1",[user_id]);
+    console.log(response.rows)
+    res.json({
+      status: 200,
+      successful: true,
+      message: "Favorites movies collected successfully",
+      body: {
+        movies: response.rows
+      }
+    })
+  } catch (error) {
+    res.json({
+      status: 400,
+      successful: false,
+      message: "Error: " + error,
+      body: {}
+    })
+  }
 }
 
 const getCover = async (req, res) => {
+  /* "https://image.tmdb.org/t/p/original/" + poster_path) */
 }
 
 module.exports = {
@@ -102,5 +183,4 @@ module.exports = {
   newFavorite,
   getFavorites,
   getBestPelis,
-  getCover,
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,16 @@
 const {Router} = require("express")
 const router = Router()
-const { getUsers, createUser, loginRequest, logoutRequest } = require("../controllers/indexController")
+const { getUsers, createUser, loginRequest, logoutRequest, getBestPelis, newFavorite, getFavorites
+} = require("../controllers/indexController")
 
 router.get("/users", getUsers)
 
 router.post("/users", createUser)
 router.post("/login", loginRequest)
 router.post("/logout", logoutRequest)
+router.get("/bestpelis", getBestPelis)
+router.post("/newfavorite", newFavorite)
+router.get("/favorites", getFavorites)
+
 
 module.exports = router
